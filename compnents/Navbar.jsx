2a3cdeabe2d90b79412f0c1a75f6c0eb7c9d567f
Abate Agegnehu <abateagegnehu574@gmail.com>
2025-03-31 23:33:26 +0300

    demo test

--- compnents/Navbar.jsx
+++ compnents/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      Navbar add new thing on git feature of navbar
+      Navbar add new thing on git feature of navbar demo test
     </div>
   )
 }
