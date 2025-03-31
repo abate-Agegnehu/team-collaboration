0fdf1aa9b8ae00ab3e0720bc5c7d32734890adb2
Abate Agegnehu <abateagegnehu574@gmail.com>
2025-03-31 23:44:53 +0300

    update

--- compnents/Navbar.jsx
+++ compnents/Navbar.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      add Navigation bar to the the file 
+     <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quo nostrum cumque explicabo iusto tempore sequi consectetur deleniti reprehenderit! Quibusdam animi doloribus totam provident. Laudantium hic fuga corrupti asperiores atque?
+     </p>
     </div>
   )
 }
